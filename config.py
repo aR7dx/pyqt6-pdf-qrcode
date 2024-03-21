bc9faab67dad509183d1c1686374fed5aec94f50
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-03-22 00:32:51 +0100

    reset config.py
    reset NAVIGATION_MODE value to 'JOYSTICK'

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DEVICE='DESKTOP' # not use
-NAVIGATION_MODE='KEYBOARD_MOUSE' # change between 'JOYSTICK' or 'KEYBOARD_MOUSE'
+NAVIGATION_MODE='JOYSTICK' # change between 'JOYSTICK' or 'KEYBOARD_MOUSE'
 SCROLL_SPEED=1 # adjust the speed of scroll as you want (only with joystick)
 OLD_APP_VERSION='v0.1.0-beta' # previous version of the app
 APP_VERSION='v0.1.1-beta' # version of the app
