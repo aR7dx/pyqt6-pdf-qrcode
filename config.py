305453595311d10e90d360730057bbaf67b274b4
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-03-22 19:05:20 +0100

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-import platform
-
-OS_NAME=str(platform.system()) # not use
 OLD_APP_VERSION='v0.1.0-beta' # previous version of the app
 APP_VERSION='v0.1.1-beta' # version of the app
 APP_NAME='pyqt6-pdf-qrcode' # name of the application
@@ -9,3 +6,4 @@ SCROLL_SPEED=1 # adjust the speed of scroll as you want (only with joystick)
 LISTE_ACCUEIL=[(330,900),(1540,100),(1750,100)] # coordonnées de la page d'accueil
 LISTE_SELECTION=[(1540,100),(1750,100)] # coordonnées de la page des crédits
 LISTE_CREDITS=[(760,615),(1170,615)] # coordonnées de la page de séléction
+PYAUTOGUI_PAUSE=0 # delai entre les actiosn du module pyautogui
