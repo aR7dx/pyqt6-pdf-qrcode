545b931bb6db8acd368df01f532a1ef7c93fa938
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-03-22 16:19:56 +0100

    update main.py

--- sources/main.py
+++ sources/main.py
@@ -11,17 +11,18 @@ from command import Worker, Worker4Keyboard # importation du gestionnaire des co
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # trouve le chemin absolu du fichier, et os.path.dirname() l'appelle une seconde fois pour obtenir le chemin du dossier parent. Ensuite, ce chemin est ajouté à sys.path, ce qui permet à Python de trouver et d'importer le module config.
 
-from config import NAVIGATION_MODE, APP_VERSION, OLD_APP_VERSION # importation de la variable "NAVIGATION_MODE" qui permet de savoir si l'on utilise le joystick ou le clavier/souris et de la variable "APP_VERSION" pour recuperer la version de l'application et "ODL_APP_VERSION" la version precedente
+from config import NAVIGATION_MODE, APP_VERSION, OLD_APP_VERSION, APP_NAME # importation de la variable "NAVIGATION_MODE" qui permet de savoir si l'on utilise le joystick ou le clavier/souris, de la variable "APP_VERSION" pour recuperer la version de l'application, "ODL_APP_VERSION" la version precedente et "APP_NAME" le nom de l'application
 
-if (APP_VERSION > OLD_APP_VERSION) != True:
-    raise Exception('Error in config.py file about "APP_VERSION" and "OLD_APP_VERSION", "APP_VERSION" must be bigger than "OLD_APP_VERSION" or it would makes no sense.')
-
+# ASSERTIONS
+assert APP_VERSION >= OLD_APP_VERSION , 'Error in config.py file about "APP_VERSION" and "OLD_APP_VERSION", "APP_VERSION" must be bigger than "OLD_APP_VERSION"'
+assert APP_VERSION != '' , 'Please give a version to the app in config.py set for example "APP_VERSION=v0.1.0" it must be bigger than "OLD_APP_VERSION'
+assert NAVIGATION_MODE == 'JOYSTICK' or NAVIGATION_MODE == 'KEYBOARD_MOUSE' , 'Error in config.py file about NAVIGATION_MODE can only be "JOYSTICK" or "KEYBOARD_MOUSE", change this setting and then restart program.'
 
 
 def main(): # création de la fonction main qui gère le lancement et la création de l'application
     app = QApplication(sys.argv) # création de l'application
     app.setOverrideCursor(QCursor(Qt.CursorShape.BlankCursor)) # rend invisible le curseur dans l'application
-    app.setApplicationName('pyqt6-pdf-qrcode') # défini le nom de l'application
+    app.setApplicationName(APP_NAME) # défini le nom de l'application
     
     win = Window() # création la fenetre
     win.show() # affichage en mode plein écran
@@ -30,18 +31,18 @@ def main(): # création de la fonction main qui gère le lancement et la créati
     if NAVIGATION_MODE == 'JOYSTICK':
         worker = Worker(win) # gestionnaire des commandes
         worker.command_signal.connect(win.command_handler) # connexion/lien/pont avec le gestionnaire des commandes
-    elif NAVIGATION_MODE == 'KEYBOARD_MOUSE':
+    
+    if NAVIGATION_MODE == 'KEYBOARD_MOUSE':
         worker = Worker4Keyboard(win)
         worker.command_signal.connect(win.command_handler)
-    else:
-        raise Exception('Error in config.py file about NAVIGATION_MODE can only be "JOYSTICK" or "KEYBOARD_MOUSE", change this setting and then restart program.')
-    
+   
     command_thread = Thread(target=worker.run) # création du thread des commandes
     command_thread.start() # lancement du thread des commandes
     print('Thread : command_thread   |   Statut : opérationnel') # affichage du statut du thread 'command'
     
     print('=====') # séparateur
-    print('Application chargée !') # statut de l'application
+    print('Application: ' + APP_NAME) # nom de l'application
+    print('Statut: chargée !') # statut de l'application
     print('version: ' + APP_VERSION) # version de l'application
     print('=====') # séparateur
     
