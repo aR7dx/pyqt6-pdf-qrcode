b9f61e490bff8e5e0a62bbd71394928ee0c46467
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-05-28 20:31:01 +0200

    update config.py

--- sources/config.py
+++ sources/config.py
@@ -2,4 +2,4 @@ OLD_APP_VERSION='v0.1.1-beta' # version précedente de l'application
 APP_VERSION='v1.0.0-stable' # version de l'application
 APP_NAME='pyqt6-pdf-qrcode' # nom de l'application
 NAVIGATION_MODE='JOYSTICK' # altérner entre 'JOYSTICK' et 'KEYBOARD_MOUSE'
-SCROLL_SPEED=0.04 # ajuster la vitesse de défilement (la valeur 0 est la plus rapide) (ne fonctionne qu'avec le mode joystick)
+SCROLL_SPEED=0.75 # ajuster la vitesse de défilement (la valeur 0 est la plus rapide) (ne fonctionne qu'avec le mode joystick)
