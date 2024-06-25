e5935e34003e9e11ffbacc95d320f69474ab8091
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-06-25 18:41:29 +0200

    update script.js

--- sources/content/html/js/script.js
+++ sources/content/html/js/script.js
@@ -2,25 +2,6 @@
 // La page github de 'qrcode.js' => 'https://github.com/davidshimjs/qrcodejs'.
 
 
-
-// const cursorOutline = document.querySelector("[data-cursor-outline]");
-
-// window.addEventListener('mousemove', function (e) {
-
-//   const posX = e.clientX;
-//   const posY = e.clientY;
-
-//   cursorOutline.style.left = `${posX}px`;
-//   cursorOutline.style.top = `${posY}px`;
-
-//   cursorOutline.animate({
-//     left: `${posX}px`,
-//     top: `${posY}px`
-//   }, { duration: 500, fill: 'forwards' });
-// });
-
-
-
 // QRCODE PANEL
 
 
