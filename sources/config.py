25c80968c6183ba71014c5e8c59104c3585c7b9e
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-06-28 16:05:40 +0200

    update config.py

--- sources/config.py
+++ sources/config.py
@@ -3,6 +3,5 @@ APP_VERSION='v1.2.0-stable' # version de l'application                          
 APP_NAME='pyqt6-pdf-qrcode' # nom de l'application                                                                                   |  (default: 'pyqt6-pdf-qrcode')
 NAVIGATION_MODE='JOYSTICK' # altérner entre 'JOYSTICK' et 'KEYBOARD_MOUSE'                                                           |  (default: 'JOYSTICK')
 SCROLL_SPEED=0.75 # ajuster la vitesse de défilement (la valeur 0 est la plus rapide) (ne fonctionne qu'avec le mode joystick)       |  (default: 0.75)
-VIDEOPLAYER_VOLUME=0.8 # volume du lecteur vidéo                                                                                     |  (default: 0.8)
 SUPPORTED_DOCUMENT_EXTENSIONS=['html', 'pdf'] # extensions de documents supportées                                                   |  (default: ['html', 'pdf'])
 SUPPORTED_VIDEO_EXTENSIONS=['mp4'] # extensions vidéo supportées                                                                     |  (default: ['mp4'])
