0885f76b3f6f1019c71ecfae89e94dd82d3a9519
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-07-03 20:23:13 +0200

    Add files via upload

---
+++ sources/packages/file.py
@@ -0,0 +1,28 @@
+import os
+
+class File():
+  def __init__(self, name):
+    self.name = name # nom du fichier cible
+    self.ext = None # extension du fichier cible
+    self.dir = None # chemin du dossier du fichier cible
+    self.path = None # chemin du fichier cible
+    
+    self.createPath()
+      
+  def __repr__(self):
+    return str(self.name)
+  
+  def __str__(self):
+    return str(self.name) + ', ' + str(self.title)
+  
+  def isExisting(self):
+    for _, _, file in os.walk(self.dir):
+      if self.name in file:
+        return True
+    print(f'\nLe fichier {self.name} n\'existe pas !\n')
+    raise False
+
+  def createPath(self):
+    self.ext = (self.name).split('.')[-1]
+    self.dir = os.path.split(os.path.abspath(__file__))[0]+r'/../content/' + str(self.ext) + "/"
+    self.path = str(self.dir) + str(self.name)
