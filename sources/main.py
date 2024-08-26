265b4a08ab221845a607f7c40f964c6d64951b85
SneaKxyz <158834512+SneaKxyz@users.noreply.github.com>
2024-08-26 14:08:46 +0200

    update main.py

--- sources/main.py
+++ sources/main.py
@@ -6,7 +6,7 @@ from PyQt6.QtCore import Qt # importation pour PyQt6
 from PyQt6.QtGui import QCursor, QIcon # importation du Cursor de PyQt6 afin d'avoir un contrôle sur ce dernier
 from threading import Thread # importation du module Thread qui permet de créer et gérer des Threads
 
-from handler import Handler # importation de l'interface graphique
+from sources.app import App # importation de l'interface graphique
 from sources.packages.command import Worker, Worker4Keyboard # importation du gestionnaire des commandes
 from config import NAVIGATION_MODE, APP_VERSION, OLD_APP_VERSION, APP_NAME # importation de la variable "NAVIGATION_MODE" qui permet de savoir si l'on utilise le joystick ou le clavier/souris, de la variable "APP_VERSION" pour recuperer la version de l'application, "ODL_APP_VERSION" la version precedente et "APP_NAME" le nom de l'application
 
@@ -20,24 +20,24 @@ def main():
     """
     Fonction main qui gère le lancement et la création de l'application.
     """
-    app = QApplication(sys.argv) # création de l'application
-    app.setApplicationName(f'{APP_NAME}  ({APP_VERSION})') # défini le nom de l'application
-    app.setWindowIcon(QIcon('./sources/content/images/icon/app_icon.svg'))
+    app_settings = QApplication(sys.argv) # création de l'application
+    app_settings.setApplicationName(f'{APP_NAME}  ({APP_VERSION})') # défini le nom de l'application
+    app_settings.setWindowIcon(QIcon('./sources/content/images/icon/app_icon.svg'))
     
-    handler = Handler() # création la fenetre
-    handler.showFullScreen() # affichage en mode plein écran
+    app = Handler() # création la fenetre
+    app.showFullScreen() # affichage en mode plein écran
 
     print('+==================='+ APP_NAME + '===================+')
     print('| Thread : handler         |   Statut : opérationnel   |') # affichage du statut du thread 'handler'
 
     if NAVIGATION_MODE == 'JOYSTICK':
-        app.setOverrideCursor(QCursor(Qt.CursorShape.BlankCursor)) # rend invisible le curseur de la souris dans l'application
-        worker = Worker(handler) # gestionnaire des commandes pour joystick
-        worker.command_signal.connect(handler.command_handler) # connecte les signaux du fichier command.py avec la fonction command_handler du fichier player.py
+        app_settings.setOverrideCursor(QCursor(Qt.CursorShape.BlankCursor)) # rend invisible le curseur de la souris dans l'application
+        worker = Worker(app) # gestionnaire des commandes pour joystick
+        worker.command_signal.connect(app.command_handler) # connecte les signaux du fichier command.py avec la fonction command_handler du fichier player.py
     
     if NAVIGATION_MODE == 'KEYBOARD_MOUSE':
-        worker = Worker4Keyboard(handler) # gestionnaire des commandes pour clavier/souris
-        worker.command_signal.connect(handler.command_handler) # connecte les signaux du fichier command.py avec la fonction command_handler du fichier player.py
+        worker = Worker4Keyboard(app) # gestionnaire des commandes pour clavier/souris
+        worker.command_signal.connect(app.command_handler) # connecte les signaux du fichier command.py avec la fonction command_handler du fichier player.py
    
     command_thread = Thread(target=worker.run) # création du thread des commandes
     command_thread.start() # lancement du thread des commandes
@@ -50,7 +50,7 @@ def main():
     print('| Statut:                  |   prêt                    |') # statut de l'application
     print('+======================================================+\n')
     
-    sys.exit(app.exec()) # execute l'application
+    sys.exit(app_settings.exec()) # execute l'application
 
 
 if __name__ == '__main__':
